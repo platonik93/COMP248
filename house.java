fac9cf1e0badbdc3cc5b0763e07ad222ffe9e5bd
Platonik <11967869+platonik93@users.noreply.github.com>
2018-11-12 17:34:30 -0500

    Create house.java

---
+++ house.java
@@ -0,0 +1,33 @@
+
+public class House {
+
+	
+	private int age;
+	private String [] type = {"Detached","Semi-Attached","Attached"};
+	private double cost;
+	
+	
+	
+	 {
+		// TODO Auto-generated method stub
+
+	 
+		
+	}
+	 
+	public double estimatePrice(String type,int age) {
+		
+		for (int x = 0;x <=5;x++) {
+			
+			double attachedCost = 100000* ((1+(1.01/1)).math.exp(5));
+			
+		}
+
+		
+		
+		return x;
+	}
+	
+	
+	
+}
